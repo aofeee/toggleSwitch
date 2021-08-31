2f05fed1566f80b84d07211dbee2ebb48b14a758
Stanislav Panov <xeqtr2009@yandex.ru>
2021-08-31 21:42:10 +0300

    first commit

--- src/main/java/ToggleSwitch.java
+++ src/main/java/ToggleSwitch.java
@@ -1,28 +1,28 @@
 public class ToggleSwitch {
 
     int count = 5;
-
+    final int TOY_SLEEP = 1500;
+    final int USER_SLEEP = 2000;
     volatile boolean tS = false;
 
     public void on() {
         while (count != 0) {
             try {
                 if (!tS) {
+                    Thread.sleep(USER_SLEEP);
                     tS = true;
                     System.out.printf("%s: Включаю тумблер\n", Thread.currentThread().getName());
                     count--;
-                    Thread.sleep(3000);
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
     }
-
     public void off() {
         while (!Thread.currentThread().isInterrupted()) {
             try {
-                Thread.sleep(3000);
+                Thread.sleep(TOY_SLEEP);
                 if (tS) {
                     tS = false;
                     System.out.printf("%s: Выключаю тумблер\n", Thread.currentThread().getName());
